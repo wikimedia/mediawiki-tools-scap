9609e8dd875d4aba0f2d40dcc59e514cdbcfe0ea
Bryan Davis <bd808@wikimedia.org>
2014-05-26 23:06:27 -0600

    Change import style for __init__.py
    Ori pointed out in I5c6b739 that he would prefer a single import listing
    all imported classes rather than a separate import per class.
    
    Change-Id: I467511acc43bb19377bc05a133a158aeba1fbaf3

--- scap/__init__.py
+++ scap/__init__.py
@@ -6,18 +6,20 @@
     configuration to a group of servers via SSH and rsync.
 
 """
-from .main import CompileWikiversions
-from .main import MWVersionsInUse
-from .main import PurgeL10nCache
-from .main import RebuildCdbs
-from .main import Scap
-from .main import SyncCommon
-from .main import SyncDblist
-from .main import SyncDir
-from .main import SyncDocroot
-from .main import SyncFile
-from .main import SyncWikiversions
-from .main import UpdateL10n
+from .main import (
+    CompileWikiversions,
+    MWVersionsInUse,
+    PurgeL10nCache,
+    RebuildCdbs,
+    Scap,
+    SyncCommon,
+    SyncDblist,
+    SyncDir,
+    SyncDocroot,
+    SyncFile,
+    SyncWikiversions,
+    UpdateL10n,
+)
 
 __all__ = (
     'CompileWikiversions',
