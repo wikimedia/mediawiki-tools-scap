0e2cd2127cecce6e9fae2b6069b0332c459bde80
Dan Duvall <dduvall@wikimedia.org>
2015-11-13 12:11:33 -0800

    Include a setup.py to help with packaging
    Summary:
    Include a setup.py to help with packaging
    
    Fixes T118504
    
    Test Plan: Run `python setup.py build`
    
    Reviewers: #release-engineering-team, mmodell
    
    Reviewed By: #release-engineering-team, mmodell
    
    Subscribers: demon
    
    Maniphest Tasks: T118504
    
    Differential Revision: https://phabricator.wikimedia.org/D45

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from glob import glob
+from distutils.core import setup
+
+authors = [('Antoine Musso', 'hashar@free.fr'),
+           ('Bryan Davis', 'bd808@wikimedia.org'),
+           ('Chad Horohoe', 'chadh@wikimedia.org'),
+           ('Dan Duvall', 'dduvall@wikimedia.org'),
+           ('Mukunda Modell', 'mmodell@wikimedia.org'),
+           ('Ori Livneh', 'ori@wikimedia.org'),
+           ('Tyler Cipriani', 'tcipriani@wikimedia.org')]
+
+setup(name='Scap',
+      version='3.0',
+      description='Deployment toolchain for Wikimedia projects',
+      author=', '.join([name for name, _ in authors]),
+      author_email=', '.join([email for _, email in authors]),
+      license='GNU GPLv3',
+      maintainer='Wikimedia Foundation Release Engineering',
+      maintainer_email='releng@wikimedia.org',
+      url='https://phabricator.wikimedia.org/diffusion/MSCA/',
+      packages=['scap'],
+      package_dir={'scap': 'scap'},
+      scripts=[s for s in glob('bin/*') if s != 'bin/refreshCdbJsonFiles'],
+      requires=[line.strip() for line in open('requirements.txt')],
+      classifiers=['Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7'])
