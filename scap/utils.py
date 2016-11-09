wikimedia/mediawiki-tools-scap

commit 6fe4d96a6c4a5aefcf88b15e9afe892652f0e88a

Off to the races (aka: clean up lock handling)
Tags: #release-engineering-team

Summary:
Right now it's very easy for two users to compete for control of the lock. The most obvious scenario is this:
* User creates the lock file with a permissive mode (say 660 or 666)
* Second user grabs the actual lock on the file, also releases the lock
* Both close their file descriptors
* Last one to try and delete the lock explodes.

This is kinda fun, because you can actually get your work done while Python tells you it all failed. Oh wait, that's confusing. So let's fix this by doing a couple of things:
* If we can't grab the lock, tell the user who owns it. Then you don't spend a minute waiting for logmsgbot to come and say things at you on IRC
* Create the lock with owner-exclusive read-write permissions. This should keep another user (other than root) from stealing your lock. This involves a couple of umask() swaps so open() will respect our requested mode.
* On the really really freaky off change that it does actually fail to kill the lock file at the end, just log it as a warning and move on. There's zilch we can do at that point and we've already done whatever work we yielded for anyway

This should fix T140914

Test Plan: Ran deploy a bunch of times while mucking about with lock files. Was able to trigger the normal case (someone else has your lock file) pretty easily and reliably

Reviewers: mmodell, #release-engineering-team, thcipriani

Reviewed By: mmodell, #release-engineering-team, thcipriani

Subscribers: thcipriani, jenkins

Differential Revision: https://phabricator.wikimedia.org/D292

scap/utils.py
@@ -20,6 +20,7 @@ import os
 import pwd
 import random
 import socket
+import stat
 import struct
 import subprocess
 import sys
@@ -286,9 +287,11 @@ def get_realm_specific_filename(filename, realm, datacenter):
     return filename
 
 
-def get_username():
+def get_username(user=None):
     """Get the username of the effective user."""
-    return pwd.getpwuid(os.getuid())[0]
+    if user is None:
+        user = os.getuid()
+    return pwd.getpwuid(user)[0]
 
 
 def human_duration(elapsed):
@@ -397,19 +400,38 @@ def lock(filename):
         raise LockFailedError('Failed to lock scap, global lock in place')
 
     lock_fd = None
+    # Steal the umask for a bit, can't rely on system
+    orig_umask = os.umask(0)
     try:
-        lock_fd = open(filename, 'w+')
+        lock_fd = os.open(filename,
+                          os.O_WRONLY | os.O_CREAT | os.O_EXCL,
+                          stat.S_IRUSR | stat.S_IWUSR)
         fcntl.lockf(lock_fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
     except IOError as e:
-        raise LockFailedError('Failed to lock %s: %s' % (filename, e))
+        if e.errno is 13 and os.path.exists(filename):
+            bad_user = get_username(os.stat(filename).st_uid)
+            details = 'current owner is "%s"' % bad_user
+        else:
+            details = e
+        raise LockFailedError('Failed to lock %s: %s' % (filename, details))
     else:
         yield
     finally:
+        # Return the umask
+        os.umask(orig_umask)
         if lock_fd:
             fcntl.lockf(lock_fd, fcntl.LOCK_UN)
             lock_fd.close()
             if os.path.exists(filename):
-                os.unlink(filename)
+                try:
+                    os.unlink(filename)
+                except IOError as e:
+                    # Someone else deleted the lock. Freaky, but we already
+                    # did our stuff so there's no point in halting execution
+                    get_logger.warning(
+                        'Huh, lock file disappeared before deletion. ' +
+                        'This is probably fine-ish :)'
+                    )
 
 
 @contextlib.contextmanager

commit eb61604251e479af87fada1b30efa0dd40eeda25

Fix bug introduced in 6fe4d96a6c4a5
os.open returns an int, not an object, use os.close instead of
lock_fd.close()

scap/utils.py
@@ -421,7 +421,7 @@ def lock(filename):
         os.umask(orig_umask)
         if lock_fd:
             fcntl.lockf(lock_fd, fcntl.LOCK_UN)
-            lock_fd.close()
+            os.close(lock_fd)
             if os.path.exists(filename):
                 try:
                     os.unlink(filename)