dae7fd15b3f62a540ba7588b35d08ded2f09e5eb
Mukunda Modell <mmodell@wikimedia.org>
2017-12-13 10:06:20 -0600

    Make it more obvious when we are using `git_rev` in scap.cfg
    Summary:
    This mostly just adds a couple of debug traces and upgrades one `debug()` to `info()`
    
    This should hopefully make it more obvious when a deployment repo is using `git_rev`
    setting in scap.cfg to deploy something like `origin/master` instead of `HEAD`.
    
    See T181661 where debugging the issue was made more difficult than necessary.
    I failed to notice that the rORESDEPLOY scap/scap.cfg had `git_rev` = `origin/master`
    but they did did not have git_upstream_submodules.
    
    I've also added docs for git_rev to repo_config.rst and reorganized
    the "available config variables" table to better group related vars.
    
    Reviewers: akosiaris, thcipriani, awight, #release-engineering-team
    
    Reviewed By: thcipriani, #release-engineering-team
    
    Tags: #release-engineering-team
    
    Maniphest Tasks: T181661
    
    Differential Revision: https://phabricator.wikimedia.org/D914

--- scap/deploy.py
+++ scap/deploy.py
@@ -634,6 +634,11 @@ class Deploy(cli.Application):
         # No revision passed on the command line, let's check the config
         if not rev:
             rev = self.config.get('git_rev')
+            use_upstream = self.config.get('git_upstream_submodules', False)
+            if rev.startswith('origin/') and not use_upstream:
+                logger.warning('You have set `git_rev` to "%s" without ' +
+                               'setting `git_upstream_submodules=True`. ' +
+                               'This could lead to unexpected behavior.' % rev)
 
         # No revision from the config or cli
         # AND we're not running a stage that we want to deploy new code
@@ -653,7 +658,7 @@ class Deploy(cli.Application):
                 timestamp = datetime.utcnow()
                 tag = git.next_deploy_tag(location=self.context.root)
                 commit = git.sha(location=self.context.root, rev=rev)
-                self.get_logger().debug('Deploying Rev: {}'.format(commit))
+                logger.info('Deploying Rev: {} = {}'.format(rev, commit))
 
                 self.config_deploy_setup(commit)
                 self.checks_setup()
