f5a199891d73612e89760aeb8bf340e26733f302
Effie Mouzeli <effie@wikimedia.org>
2019-03-22 12:32:27 +0200

    Add more tests for main
    Bug: T216518
    Change-Id: I74c9d0434bc321266f38d126d5962efd2bc0dd4e

--- tests/scap/test_main.py
+++ tests/scap/test_main.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 import pytest
-
+import os
 from scap import cli
 
 
@@ -47,3 +47,51 @@ def test_increment_stat(cmd, mocker):
     mocker.resetall()
     cmd.increment_stat('sync-file', all_stat=False, value=5)
     stats.increment.assert_called_once_with('deploy.sync-file', 5)
+
+
+api_canaries = ['pigeon.w.o', 'crow.w.o', 'cormorant.w.o', 'flamingo.w.o']
+app_canaries = ['birdy.w.o', 'hawk.w.o', 'duck.w.o', 'chick.w.o']
+
+
+def test__get_api_canary_list(cmd, mocker):
+    tl = mocker.patch('scap.targets.get')
+    tl.return_value.all = api_canaries
+    assert cmd._get_api_canary_list() == api_canaries
+
+
+def test__get_app_canary_list(cmd, mocker):
+    tl = mocker.patch('scap.targets.get')
+    tl.return_value.all = app_canaries
+    assert cmd._get_app_canary_list() == app_canaries
+
+
+def test__get_canary_list(cmd, mocker):
+    canary_targets = mocker.patch('scap.targets.get')
+
+    def side_effect_cb(label, config):
+        tgt = mocker.MagicMock()
+        if label == 'dsh_api_canaries':
+            all = api_canaries
+        else:
+            all = app_canaries
+        tgt.all = all
+        return tgt
+    canary_targets.side_effect = side_effect_cb
+    assert sorted(cmd._get_canary_list()) == sorted(app_canaries+api_canaries)
+
+
+@pytest.mark.parametrize('lock_exists', [True, False])
+def test__check_sync_flag(cmd, mocker, lock_exists):
+    isthere = mocker.patch('os.path.exists')
+    ms = mocker.patch('os.stat')
+    ms.return_value = os.stat('/bin/bash')
+    mu = mocker.patch('pwd.getpwuid')
+    mu.return_value.pw_name = 'scap'
+    if lock_exists:
+        isthere.return_value = lock_exists
+        with pytest.raises(IOError) as excinfo:
+            cmd._check_sync_flag()
+        assert 'Blocked' in str(excinfo.value)
+    else:
+        isthere.return_value = lock_exists
+        cmd._check_sync_flag()
