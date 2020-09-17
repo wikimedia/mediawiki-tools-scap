74020b3956060e397c670950b66a528ff6d30ad8
Ahmon Dancy <adancy@wikimedia.org>
2020-09-17 11:22:53 -0700

    update-wikiversions: Use staging dir wikiversions to update php link
    scap update-wikiversions performs a read/modify/write of
    /srv/mediawiki-staging/wikiversions.json.  Then it updates the 'php'
    symlink to point to the newest version found in
    /srv/mediawiki/wikiversions.json (the file that wasn't modified).
    Based on the comment for function update_branch_pointer:
    
    """Swap the php symlink over to the new version as well, if needed."""
    
    it seems clear that it intended to use the updated version info.  This
    commit fixes that by passing 'stage' to the call to
    active_wikiversions.  This makes it read the just-updated
    /srv/mediawiki-staging/wikiversions.json file.
    
    Tested in train-dev environment.
    
    Change-Id: I386bfe8f7418195ce67e12e37e74260b31e833df

--- scap/plugins/updatewikiversions.py
+++ scap/plugins/updatewikiversions.py
@@ -72,7 +72,7 @@ class UpdateWikiversions(cli.Application):
 
     def update_branch_pointer(self):
         """Swap the php symlink over to the new version as well, if needed."""
-        cur_version = self.active_wikiversions().popitem()[0]
+        cur_version = self.active_wikiversions('stage').popitem()[0]
 
         real_path = os.path.join(
             self.config['stage_dir'], 'php-%s' % cur_version)
