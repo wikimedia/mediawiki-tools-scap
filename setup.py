d83f1342c833b5ec0f33ce250369e04b8bfe52f5
Ahmon Dancy <adancy@wikimedia.org>
2021-09-14 11:53:04 -0700

    Release 4.0.0-1
    Change-Id: I92451b9059b5515b799defdc647b17fe602594be

--- setup.py
+++ setup.py
@@ -61,8 +61,7 @@ setup(
     classifiers=[
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     ],
 )
