9caf2c5f6d3bd4a69a9695f390f65fe2f39bbc20
Jaime Nuche <jnuche@wikimedia.org>
2022-05-19 10:44:58 +0200

    bootstrap: add script to install/update local version of scap
    Creates a self-contained scap Python3 venv in the user's HOME
    
    Bug: T307086
    Change-Id: Ibbd18f19cdd85b3dc9b7e285ff7ba639ae505fa7

---
+++ bootstrap/install_local_version.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+# This script can be used to create a local, self-contained, fresh installation of scap from a local
+# copy of the source code; or to update an already existing installation. It is meant for the
+# following use cases:
+#    * Bootstrap/stage a scap installation on a deploy server
+#    * Allow a RelEng operator to create their own installation in their home
+#
+# The script can be used to install versions >4.7.1 of scap
+
+set -eu -o pipefail
+
+function usage {
+  cat <<HERE
+
+   Usage: $0 [-u <USERNAME>] [-t <TAG>] <SCAP_SOURCE_DIR>
+
+   Uses a scap git repository at <SCAP_SOURCE_DIR> to create/update a self-contained scap Python3
+   venv in the user's HOME at $HOME/scap. If <USERNAME> is not passed, the environment variable USER
+   is taken instead. An optional git <TAG> can be passed to specify the code revision to use when
+   creating the venv.
+
+   Note the user running this script needs to have permissions:
+     * To read <SCAP_SOURCE_DIR>
+     * To sudo as <USERNAME> (unless the user is already <USERNAME>)
+     * If a tag is specified, permissions to write <SCAP_SOURCE_DIR> and to access the git remote
+     pointed to by <SCAP_SOURCE_DIR>
+
+HERE
+}
+
+function log {
+  echo -e "INFO: $1"
+}
+
+function fail {
+  echo -e "ERROR: $1. Aborting"
+  exit 1
+}
+
+function git_scap {
+  git -C "$SCAP_SOURCE_DIR" "$@"
+}
+
+function parseArgs {
+  INSTALL_USER=
+  TAG=
+
+  while getopts 'u:t:' opt; do
+    case "$opt" in
+      u)
+        INSTALL_USER=$OPTARG
+        ;;
+      t)
+        TAG=$OPTARG
+        ;;
+      *)
+        usage
+        exit
+        ;;
+    esac
+  done
+  shift $((OPTIND - 1))
+
+  (($# < 1)) && usage && exit
+
+  SCAP_SOURCE_DIR=$1
+
+  if [ -z "$INSTALL_USER" ]; then
+    if [ -z "${USER:-}" ]; then
+      fail "No user specified and USER var env is not set"
+    fi
+    INSTALL_USER=$USER
+  fi
+}
+
+function verify_source_dir {
+ if ! git_scap config remote.origin.url 2>/dev/null | grep -q mediawiki/tools/scap; then
+   fail "Specified source path \"$SCAP_SOURCE_DIR\" does not seem to be a scap repository"
+ fi
+}
+
+function verify_user {
+  if ! id "$INSTALL_USER" &>/dev/null; then
+    fail "Unknown user \"$INSTALL_USER\""
+  fi
+}
+
+function verify_tag {
+  if [ -n "$TAG" ]; then
+    git_scap fetch --prune
+    if ! git_scap rev-parse tags/"$TAG" &>/dev/null; then
+     fail "Specified tag \"$TAG\" is not recognized"
+    fi
+  fi
+}
+
+function verify_args {
+  verify_source_dir
+  verify_user
+  verify_tag
+}
+
+function check_out_tag {
+  git_scap -c advice.detachedHead=false checkout "$TAG"
+  # The trap ensures the script restores the git repo to the original checkout it found before
+  # running. The call generates several lines of output, so we use the grep filter to trim it down
+  # to the most informative of those
+  trap "git_scap checkout - 2>&1 | grep -i switched" EXIT
+
+  log "Tag \"$TAG\" checked out"
+}
+
+function create_scap_venv_for_user {
+  local USER_HOME
+  USER_HOME=$(eval echo "~$INSTALL_USER")
+  local VENV_DIR=${USER_HOME}/scap
+  local AS_USER=
+  local http_proxy=
+  local https_proxy=
+
+  if dnsdomainname | grep -q wmnet; then
+    local SUBDOMAIN
+    SUBDOMAIN=$(dnsdomainname | cut -d. -f1)
+    export http_proxy="http://webproxy.${SUBDOMAIN}.wmnet:8080"
+    export https_proxy=$http_proxy
+  fi
+
+  if [ ! "$INSTALL_USER" = "$(id -un)" ]; then
+    if sudo -n -u "$INSTALL_USER" id &>/dev/null; then
+      AS_USER="sudo -su $INSTALL_USER --preserve-env=http_proxy,https_proxy"
+    else
+      fail "Could not sudo to user \"$INSTALL_USER\""
+    fi
+  fi
+
+  $AS_USER python3 -m venv "$VENV_DIR"
+  $AS_USER "${VENV_DIR}"/bin/pip3 -v install wheel==0.37.1
+  $AS_USER "${VENV_DIR}"/bin/pip3 install --upgrade "$SCAP_SOURCE_DIR"
+
+  if [ -n "$TAG" ]; then
+    log "Scap \"$TAG\" successfully installed/updated at \"$VENV_DIR\""
+  else
+    log "Scap successfully installed/updated at \"$VENV_DIR\""
+  fi
+}
+
+function install_scap {
+  [ -n "$TAG" ] && check_out_tag
+  create_scap_venv_for_user
+}
+
+parseArgs "$@"
+verify_args
+install_scap
