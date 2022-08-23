9c0288edecb3555734b3c54a262c4d5c4136d77a
Ahmon Dancy <adancy@wikimedia.org>
2022-08-23 11:48:09 -0700

    Add "scap php-fpm-restart"
    "scap php-fpm-restart" will restart php-fpm on all mediawiki targets.
    This is useful in case the php-fpm-restart phase of a previous sync
    failed.  It is also useful for debugging.
    
    Bug: T302631
    Bug: T266055
    Change-Id: I675dcbc22a0c350656937c76f7f8382da2e4ac2e

---
+++ scap/plugins/php-fpm-restart.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from scap import cli, lock, main
+
+
+@cli.command("php-fpm-restart", help="Restart php-fpm on all mediawiki targets")
+class PhpRpmRestartCmd(main.AbstractSync):
+    @cli.argument("message", nargs="*", help="Log message for SAL")
+    def main(self, *extra_args):
+        self.arguments.force = False
+
+        self._assert_auth_sock()
+
+        with lock.Lock(self.get_lock_file(), self.arguments.message):
+            self.announce("Starting php-fpm-restarts")
+            self._restart_php()
+            self.announce("Finished php-fpm-restarts")
