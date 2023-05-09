68dc6924b21e1b69bcddf81cb51191afce74fafa
Jeena Huneidi <jhuneidi@wikimedia.org>
2023-05-09 14:51:16 -0700

    Refactoring, depends on backport wrong branch

--- tests/scap/integration/test_backport.py
+++ tests/scap/integration/test_backport.py
@@ -5,7 +5,11 @@ import subprocess
 import time
 from datetime import datetime
 import unittest
+from io import StringIO
+from unittest.mock import patch
+import pdb
 
+import scap.cli
 import scap.utils
 from scap.plugins.gerrit import GerritSession
 from scap.runcmd import gitcmd
@@ -106,7 +110,7 @@ class BackportsTestHelper:
                                "+2"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
     def approve_and_wait_for_merge(self, change_url):
-        """ as named  """
+        """ approves the change and returns after change has merged  """
         self.approve_change(change_url)
         logging.info("Waiting for %s to merge...", change_url)
 
@@ -116,16 +120,17 @@ class BackportsTestHelper:
 
     def scap_backport(self, args_list):
         logging.info("Running scap backport --yes --stop-before-sync %s" % ' '.join(args_list))
-        subprocess.check_call(["scap", "backport", "--yes", "--stop-before-sync"] + args_list)
+        subprocess.check_call(["scap", "backport", "-Dlog_test:True", "--yes", "--stop-before-sync"] + args_list)
 
-    def write_to_top_of_file(self, filepath, message):
-        with open(filepath, 'r') as f:
-            contents = f.read()
-        self.write_to_file("w", filepath, message + contents)
-
-    def write_to_file(self, mode, filepath, message):
-        with open(filepath, mode) as f:
-            f.writelines(message)
+    def write_to_file(self, filepath, message, append=True):
+        if append:
+            with open(filepath, "a") as f:
+                f.writelines(message)
+        else:
+            with open(filepath, 'r') as f:
+                contents = f.read()
+            with open(filepath, "w") as f:
+                f.writelines(message + contents)
 
     def git_command(self, repo, command):
         subprocess.check_call(["git", "-C"] + [repo] + command)
@@ -137,14 +142,17 @@ class BackportsTestHelper:
     def get_change_id(self, repo):
         return re.search(r"(?m)Change-Id:.+$", gitcmd("-C", repo, "log", "-1")).group().split(" ")[1]
 
-    def config_backport(self, mode):
-        """test that a config change can be backported"""
+    def change_and_push_file(self, repo, branch, file, text, commit_message, append=True):
+        self.write_to_file(file, text, append)
+        self.git_commit(repo, commit_message, file)
+        return self.push_and_collect_url(repo, branch)
+
+    def config_backport(self, mode, branch="train-dev"):
+        """ makes a change to the operations/config repo and backports """
         announce("Config change test: %s" % mode)
         readme_path = self.mwconfig_dir + "/README"
         text = "\nscap backport test config change: " + datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.write_to_file("a", readme_path, text)
-        self.git_commit(self.mwconfig_dir, text, readme_path)
-        change_url = self.push_and_collect_url(self.mwconfig_dir, "train-dev")
+        change_url = self.change_and_push_file(self.mwconfig_dir, branch, readme_path, text, "config backport test")
 
         if mode == "already_merged":
             logging.info("Merging the change before scap backport to verify that this is okay")
@@ -152,69 +160,69 @@ class BackportsTestHelper:
 
         self.scap_backport([change_url])
 
-    def core_backport(self):
-        """tests a merge commit"""
+    def merge_commit_backport(self):
+        """ makes and backports a change that should result in a merge commit """
         announce("Testing mediawiki/core changes (two independent commits resulting in a merge commit)")
         developers_path = self.mwcore_dir + "/DEVELOPERS.md"
         text = "Added by scap backport on " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
-        self.write_to_top_of_file(developers_path, text)
-
-        self.git_commit(self.mwcore_dir, "scap backport testing: add a line to top of DEVELOPERS.md", developers_path)
-        change_url1 = self.push_and_collect_url(self.mwcore_dir, self.mwbranch)
+        change_url1 = self.change_and_push_file(self.mwcore_dir, self.mwbranch, developers_path, text,
+                                                "scap backport testing: add a line to top of DEVELOPERS.md", False)
 
         self.git_command(self.mwcore_dir, ["reset", "--hard", "HEAD~1"])
         text = "\nAdded by scap backport on " + datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.write_to_file("a", developers_path, text)
-        self.git_commit(self.mwcore_dir, "scap backport testing: add line to bottom of DEVELOPERS.md\n "
-                                         "This is expected to result in a merge commit", developers_path)
-        change_url2 = self.push_and_collect_url(self.mwcore_dir, self.mwbranch)
+        change_url2 = self.change_and_push_file(self.mwcore_dir, self.mwbranch, developers_path, text,
+                                                "scap backport testing: add line to bottom of DEVELOPERS.md\n "
+                                                "This is expected to result in a merge commit")
 
         self.scap_backport([change_url1])
         self.scap_backport([change_url2])
 
-    def relation_chain_backport_fails(self):
-        """Tests backporting relation chains out of order"""
-        announce("Testing the latest commit in a relation chain cannot be backported alone")
+    def setup_relation_chains(self):
+        """ creates relation chain changesets and stores change urls """
         credits_path = self.mwcore_dir + "/CREDITS"
         text = "Added by scap backport on " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
-        self.write_to_top_of_file(credits_path, text)
-        self.git_commit(self.mwcore_dir, "scap backport testing: add line to top of CREDITS", credits_path)
-        change_url1 = self.push_and_collect_url(self.mwcore_dir, self.mwbranch)
+        change_url1 = self.change_and_push_file(self.mwcore_dir, self.mwbranch, credits_path, text,
+                                                "scap backport testing: add line to top of CREDITS", False)
 
         text = "\nAdded by scap backport on " + datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.write_to_file("a", credits_path, text)
-        self.git_commit(self.mwcore_dir, "scap backport testing: add line to bottom of CREDITS\n"
-                                         "This is expected to result in a relation chain", credits_path)
-        change_url2 = self.push_and_collect_url(self.mwcore_dir, self.mwbranch)
+        change_url2 = self.change_and_push_file(self.mwcore_dir, self.mwbranch, credits_path, text,
+                                                "scap backport testing: add line to bottom of CREDITS\n"
+                                                "This is expected to result in a relation chain")
 
         self.relation_change_urls = [change_url1, change_url2]
-        self.scap_backport([change_url2])
+
+    def relation_chain_backport_fails(self):
+        """ attempts to backport stored relation chain commits out of order """
+        announce("Testing the latest commit in a relation chain cannot be backported alone")
+        self.scap_backport([self.relation_change_urls[1]])
 
     def relation_chain_backport_succeeds(self):
-        """Tests backporting relation chains"""
+        """ backports stored relation chain urls """
         announce("Testing relation chains can be backported")
         self.scap_backport(self.relation_change_urls)
 
-    def depends_on_backport_fails(self):
-        announce("Testing commit with Depends-On cannot be backported alone")
+    def setup_depends_ons(self):
+        """ creates Depends-On changesets and stores change urls """
         faq_path = self.mwcore_dir + "/FAQ"
         text = "Added by scap backport on " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
-        self.write_to_top_of_file(faq_path, text)
-        self.git_commit(self.mwcore_dir, "scap backport testing: add line to top of FAQ", faq_path)
-        change_url1 = self.push_and_collect_url(self.mwcore_dir, self.mwbranch)
+        change_url1 = self.change_and_push_file(self.mwcore_dir, self.mwbranch, faq_path, text,
+                                                "scap backport testing: add line to top of FAQ", False)
         change_id = self.get_change_id(self.mwcore_dir)
 
         self.git_command(self.mwcore_dir, ["reset", "--hard", "HEAD~1"])
 
         text = "\nAdded by scap backport on " + datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.write_to_file("a", faq_path, text)
-        self.git_commit(self.mwcore_dir, "scap backport testing: add Depends-On\n\nDepends-On: %s" % change_id,
-                        faq_path)
-        change_url2 = self.push_and_collect_url(self.mwcore_dir, self.mwbranch)
+        change_url2 = self.change_and_push_file(self.mwcore_dir, self.mwbranch, faq_path, text,
+                                                "scap backport testing: add Depends-On\n\nDepends-On: %s" % change_id)
         self.depends_on_change_urls = [change_url1, change_url2]
-        self.scap_backport([change_url2])
+
+    def depends_on_backport_fails(self):
+        """ attempts to backport a change which depends on an unmerged change """
+        announce("Testing commit with Depends-On cannot be backported alone")
+        self.scap_backport([self.depends_on_change_urls[1]])
 
     def depends_on_backport_succeeds(self):
+        """ backports depends-on changes """
         announce("Testing simultaneous merge of commit and dependency")
         self.scap_backport(self.depends_on_change_urls)
         change_id = self.get_change_id(self.mwcore_dir)
@@ -222,18 +230,16 @@ class BackportsTestHelper:
         announce("Testing merge of commit with already merged dependency")
         faq_path = self.mwcore_dir + "/FAQ"
         text = "\nAdded by scap backport on " + datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.write_to_file("a", faq_path, text)
-        self.git_commit(self.mwcore_dir, "scap backport testing: add Depends-On part 2\n\nDepends-On: %s" % change_id,
-                        faq_path)
-        change_url1 = self.push_and_collect_url(self.mwcore_dir, self.mwbranch)
+        change_url1 = self.change_and_push_file(self.mwcore_dir, self.mwbranch, faq_path, text,
+                                                "scap backport testing: add Depends-On part 2\n\nDepends-On: %s"
+                                                % change_id)
         self.scap_backport([change_url1])
 
         readme_path = self.mwcore_dir + "/README.md"
         announce("Testing dependency that shares a change_id")
         text = "Added by scap backport on " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
-        self.write_to_top_of_file(readme_path, text)
-        self.git_commit(self.mwcore_dir, "scap backport testing: add line to top of README.md", readme_path)
-        change_url1 = self.push_and_collect_url(self.mwcore_dir, self.mwbranch)
+        change_url1 = self.change_and_push_file(self.mwcore_dir, self.mwbranch, readme_path, text,
+                                                "scap backport testing: add line to top of README.md", False)
         commit = subprocess.check_output(["git", "-C", self.mwcore_dir, "rev-parse", "HEAD"], text=True).strip()
         change_id = self.get_change_id(self.mwcore_dir)
         self.git_command(self.mwcore_dir, ["reset", "--hard", "HEAD~1"])
@@ -241,32 +247,55 @@ class BackportsTestHelper:
 
         # cherry-pick change to another branch
         self.git_command(self.mwcore_dir, ["cherry-pick", commit])
-        change_url2 = self.push_and_collect_url(self.mwcore_dir, "master")
+        self.push_and_collect_url(self.mwcore_dir, "master")
         self.git_command(self.mwcore_dir, ["reset", "--hard", "HEAD~1"])
 
         # make a new change depending on cherry-pick
         text = "\nAdded by scap backport on " + datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.write_to_file("a", readme_path, text)
-        self.git_commit(self.mwcore_dir, "scap backport testing: add Depends-On\n\nDepends-On: %s" % change_id,
-                        readme_path)
-        change_url3 = self.push_and_collect_url(self.mwcore_dir, self.mwbranch)
+        change_url3 = self.change_and_push_file(self.mwcore_dir, self.mwbranch, readme_path, text,
+                                                "scap backport testing: add Depends-On\n\nDepends-On: %s" % change_id)
         self.scap_backport([change_url1, change_url3])
 
+    def depends_on_backport_wrong_branch(self):
+        self.git_command(self.mwconfig_dir, ["checkout", "master"])
+        self.git_command(self.mwconfig_dir, ["pull", "origin", "master", "--rebase"])
+        readme_path = self.mwconfig_dir + "/README"
+        text = "Added by scap backport on " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
+        change_url = self.change_and_push_file(self.mwconfig_dir, "master", readme_path, text, "Depends-On testing 3")
+        change_id = self.get_change_id(self.mwconfig_dir)
+        self.scap_backport([change_url])
+
+        self.git_command(self.mwconfig_dir, ["reset", "--hard", "HEAD~1"])
+        self.git_command(self.mwconfig_dir, ["checkout", "train-dev"])
+        text = "Added by scap backport on " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
+        change_url = self.change_and_push_file(self.mwconfig_dir, "train-dev", readme_path, text,
+                                               "Depends-On testing 3\n\nDepends-On: %s" % change_id, False)
+
+        self.scap_backport([change_url])
+
+
+        '''install_path = self.mwcore_dir + "/INSTALL"
+        text = "Added by scap backport on " + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "\n"
+        self.write_to_top_of_file(install_path, text)
+        self.git_commit(self.mwcore_dir, "scap backport testing: add line to top of INSTALL", install_path)
+        change_url = self.push_and_collect_url(self.mwcore_dir, "master")
+        self.git_command(self.mwcore_dir, ["reset", "--hard", "HEAD~1"])
+        self.scap_backport([change_url])'''
+
     def extension_backport(self):
-        """Tests for a submodule update commit
+        """ backports an extension change with submodule update commit and stores the change url
             FIXME: it might be interesting to also test for submodule _and_ merge commit
         """
         announce("Testing mediawiki/extensions/GrowthExperiments change")
         copying_path = self.mwgrowthexperiments_dir + "/COPYING"
         text = "\nAdded by scap backport on " + datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.write_to_file("a", copying_path, text)
-        self.git_commit(self.mwgrowthexperiments_dir, "scap backport testing: add line to bottom of COPYING",
-                        copying_path)
-        change_url = self.push_and_collect_url(self.mwgrowthexperiments_dir, self.mwbranch)
+        change_url = self.change_and_push_file(self.mwgrowthexperiments_dir, self.mwbranch, copying_path, text,
+                                               "scap backport testing: add line to bottom of COPYING")
         self.scap_backport([change_url])
         self.extension_change_url = change_url
 
     def extension_revert(self):
+        """ reverts a change """
         announce("mediawiki/extensions/GrowthExperiments revert change")
         self.scap_backport(["--revert", self.extension_change_url])
         return subprocess.check_output(["git", "-C", self.mwgrowthexperiments_dir, "rev-list", "@{u}..HEAD", "--count"],
@@ -286,25 +315,36 @@ class TestBackports(unittest.TestCase):
         self.backports_test_helper.config_backport("normal")
         self.backports_test_helper.config_backport("already_merged")
 
-    def test_core_changes(self):
-        self.backports_test_helper.core_backport()
+    def test_merge_commits(self):
+        self.backports_test_helper.merge_commit_backport()
 
     def test_extension(self):
         self.backports_test_helper.extension_backport()
 
     def test_relation_chains(self):
+        self.backports_test_helper.setup_relation_chains()
         with self.assertRaises(Exception) as context:
             self.backports_test_helper.relation_chain_backport_fails()
             self.assertTrue("not merged or scheduled for backport" in context.exception)
 
         self.backports_test_helper.relation_chain_backport_succeeds()
 
-    def test_depends_ons(self):
+    @unittest.mock.patch.object(logging.getLogger('scap.backport'), 'warning')
+    def test_depends_ons(self, mock_warn):
+        self.backports_test_helper.setup_depends_ons()
         with self.assertRaises(Exception) as context:
             self.backports_test_helper.depends_on_backport_fails()
             self.assertTrue("not merged or scheduled for backport" in context.exception)
 
         self.backports_test_helper.depends_on_backport_succeeds()
+
+        with patch('scap.utils.input', create=True, return_value='n'):
+            # with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            self.backports_test_helper.depends_on_backport_wrong_branch()
+            self.assertTrue("included in any mediawiki production branch" in mock_warn.records[-1].message)
+            self.assertTrue(mock_warn.records[-1].levelname == "WARN")
+            # pdb.set_trace()
+        # self.assertTrue("included in any mediawiki production branch" in mock_stdout.getvalue())
 
     def test_revert(self):
         self.assertEqual(self.backports_test_helper.extension_revert(), '1')
