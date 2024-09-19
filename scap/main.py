8a8bbc291f34601efe573d51b1e73e57b345daa6
Dan Duvall <dduvall@wikimedia.org>
2024-09-19 11:32:56 -0700

    mwscript: Containerize MediaWiki script execution
    (Rewrite of 9ba1949d2e00a9ba4b0dfead5ed48606f7e7eff9)
    (Revert of revert 8d4b4f6ae64d495110fa20af426bba310cb4aec2)
    
    Run all MediaWiki PHP scripts using Docker.
    
    This will allow us to run certain scap commands in environments other
    than the deployment host (such as release hosts) without installing a
    large number of MediaWiki dependencies.
    
    Provide a new `mwscript` module that implements mwscript and shell
    execution using Docker.
    
    The new `mwscript.run` and `mwscript.run_shell` functions call out to
    `scap mwscript` and `scap mwshell` subcommands which in turn execute
    mwscript and shell commands via Docker.
    
    If the new `docker_user` configuration is set, the subcommands are
    executed via sudo as that user. This extra layer of invocation allows
    limited access to the Docker socket without granting full access to the
    runtime user.
    
    Refactor `tasks` and `main` modules to use `mwscript.run` and
    `mwscript.run_shell` instead of `sudo_check_call`.
    
    Bug: T369115
    Change-Id: I9a7520d43836669e9720aa5fe1f1b88443fdf259

--- scap/main.py
+++ scap/main.py
@@ -42,6 +42,7 @@ import scap.lint as lint
 import scap.lock as lock
 import scap.log as log
 import scap.logstash_checker as logstash_checker
+import scap.mwscript as mwscript
 import scap.php_fpm as php_fpm
 import scap.ssh as ssh
 import scap.targets as targets
@@ -50,7 +51,6 @@ import scap.utils as utils
 import scap.version as scapversion
 from scap import ansi, history
 from scap.kubernetes import K8sOps, TEST_SERVERS, CANARIES, PRODUCTION, STAGES
-from scap.runcmd import mwscript
 
 
 @dataclass
@@ -292,7 +292,7 @@ class AbstractSync(cli.Application):
         if all_stat:
             self.get_stats().increment("deploy.all", value)
 
-    def get_keyholder_key(self):
+    def get_keyholder_key(self, *args, **kwargs):
         """
         Returns scap2-specific deploy key
 
@@ -303,7 +303,7 @@ class AbstractSync(cli.Application):
         if key:
             return key
 
-        return super().get_keyholder_key()
+        return super().get_keyholder_key(*args, **kwargs)
 
     def _before_cluster_sync(self):
         pass
@@ -380,11 +380,16 @@ class AbstractSync(cli.Application):
         ).items():
             logger.debug("Testing {} with eval.php using {}".format(version, wikidb))
             with utils.suppress_backtrace():
-                stderr = mwscript("eval.php", "--wiki", wikidb)
-                if stderr:
+                proc = mwscript.run(
+                    self,
+                    "eval.php",
+                    wiki=wikidb,
+                    check=False,
+                )
+                if proc.stderr:
                     raise SystemExit(
                         "'mwscript eval.php --wiki {}' generated unexpected output: {}".format(
-                            wikidb, stderr
+                            wikidb, proc.stderr
                         )
                     )
 
@@ -1089,7 +1094,7 @@ class ScapWorld(AbstractSync):
         self._after_sync_rebuild_cdbs(target_hosts)
         self._after_sync_sync_wikiversions(target_hosts)
         self._restart_php()
-        tasks.clear_message_blobs(self.config)
+        tasks.clear_message_blobs(self)
 
     def _after_lock_release(self):
         self.announce(
--- scap/tasks.py
+++ scap/tasks.py
@@ -29,7 +29,6 @@ import logging
 import multiprocessing
 import os
 import pwd
-import re
 import socket
 import subprocess
 import sys
@@ -38,6 +37,7 @@ import time
 import scap.cdblib as cdblib
 import scap.git as git
 import scap.log as log
+import scap.mwscript as mwscript
 import scap.ssh as ssh
 import scap.utils as utils
 
@@ -488,6 +488,7 @@ def update_l10n_cdb_wrapper(args, logger=None):
 
 
 def _call_rebuildLocalisationCache(
+    app,
     version,
     out_dir,
     use_cores=1,
@@ -500,6 +501,7 @@ def _call_rebuildLocalisationCache(
     """
     Helper for update_localization_cache.
 
+    :param app: Scap cli.Application
     :param version: The train version to build l10n for
     :param out_dir: The output directory
     :param use_cores: The number of cores to run in
@@ -515,34 +517,48 @@ def _call_rebuildLocalisationCache(
         lang = os.getenv("SCAP_MW_LANG")
 
     def _rebuild(store_class, file_extension):
-        logging.info("Running rebuildLocalisationCache.php as www-data")
+        logging.info("Running rebuildLocalisationCache.php")
         # Passing --skip-message-purge for T263872 (if delay_messageblobstore_purge feature
         # flag is enabled).
         # Note: mwscript runs maintenance scripts from /srv/mediawiki-staging if it exists,
         # otherwise it falls back to /srv/mediawiki.  If rebuildLocalisationCache.php is run
         # from /srv/mediawiki-staging (the usual case), it will update files in
         # /srv/mediawiki-staging/php-<vers>/cache/l10n
-        utils.sudo_check_call(
-            "www-data",
-            "/usr/local/bin/mwscript rebuildLocalisationCache.php "
-            "--wiki=aawiki "
-            '--force-version "%(version)s" '
-            "--no-progress "
-            "--store-class=%(store_class)s "
-            "--threads=%(use_cores)s %(lang)s %(force)s %(quiet)s %(skip_message_purge)s"
-            % {
-                "version": version,
-                "store_class": store_class,
-                "use_cores": use_cores,
-                "lang": "--lang " + lang if lang else "",
-                "force": "--force" if force else "",
-                "quiet": "--quiet" if quiet else "",
-                "skip_message_purge": "--skip-message-purge"
-                if delay_messageblobstore_purge
-                else "",
-            },
-            logLevel=logging.INFO,
-        )
+        args = [
+            "--no-progress",
+            "--store-class",
+            store_class,
+            "--threads",
+            use_cores,
+        ]
+
+        if lang:
+            args += ["--lang", lang]
+        if force:
+            args += ["--force"]
+        if quiet:
+            args += ["--quiet"]
+
+        network = True
+        # If we're delaying the message purge, we don't strictly need the
+        # network or database access
+        if delay_messageblobstore_purge:
+            network = False
+            args += [
+                "--no-database",
+                "--skip-message-purge",
+            ]
+
+        with log.pipe() as out:
+            mwscript.run(
+                app,
+                "rebuildLocalisationCache.php",
+                *args,
+                version=version,
+                network=network,
+                stdout=out,
+            )
+        mwscript.run_shell(app, "chmod 0664 {}/*.cdb", out_dir)
 
     _rebuild("LCStoreCDB", "cdb")
 
@@ -602,25 +618,19 @@ def update_localization_cache(version, app, logger=None, json=True):
 
     if os.path.exists(cache_dir):
         # Clean up cruft from any prior interrupted run.
-        utils.sudo_check_call(
-            "www-data", "rm -f {}".format(os.path.join(cache_dir, "*.tmp.*"))
-        )
+        mwscript.run_shell(app, "rm -f {}/*.tmp.*", cache_dir)
 
     if not os.path.exists(os.path.join(cache_dir, "l10n_cache-en.cdb")):
         # mergeMessageFileList.php needs a l10n file
         logger.info("Bootstrapping l10n cache for %s", version)
         _call_rebuildLocalisationCache(
-            version, cache_dir, use_cores, cfg["php_l10n"], lang="en", quiet=True
+            app, version, cache_dir, use_cores, cfg["php_l10n"], lang="en", quiet=True
         )
         # Force subsequent cache rebuild to overwrite bootstrap version
         force_rebuild = True
 
     logger.info("Updating ExtensionMessages-%s.php", version)
-    new_extension_messages = (
-        subprocess.check_output(["sudo", "-u", "www-data", "-n", "--", "/bin/mktemp"])
-        .decode()
-        .strip()
-    )
+    new_extension_messages = mwscript.run_shell(app, "mktemp").stdout.strip()
 
     # attempt to read extension-list from the branch instead of wmf-config
     ext_list = os.path.join(cfg["stage_dir"], "php-%s" % version, "extension-list")
@@ -629,34 +639,30 @@ def update_localization_cache(version, app, logger=None, json=True):
         # fall back to the old location in wmf-config
         ext_list = "%s/wmf-config/extension-list" % cfg["stage_dir"]
 
-    merge_message_file_list_command = (
-        "/usr/local/bin/mwscript mergeMessageFileList.php "
-        '--wiki=aawiki --force-version "%s" --list-file="%s" '
-        '--output="%s"' % (version, ext_list, new_extension_messages)
+    mwscript.run(
+        app,
+        "mergeMessageFileList.php",
+        "--list-file",
+        ext_list,
+        "--output",
+        new_extension_messages,
+        version=version,
+        check_warnings=True,
     )
-    diags = utils.sudo_check_call("www-data", merge_message_file_list_command)
-    utils.sudo_check_call("www-data", 'chmod 0664 "%s"' % new_extension_messages)
 
     try:
-        # Check for PHP notices/warnings in mergeMessageFileList.php output.
-        if re.search(r"PHP (Notice|Warning)", diags):
-            raise SystemExit(
-                f"""
-{merge_message_file_list_command} generated PHP notices/warnings:
-{diags}
-"""
-            )
-
+        mwscript.run_shell(app, "chmod 0664 {}", new_extension_messages)
         with open(new_extension_messages) as f:
             utils.write_file_if_needed(extension_messages, f.read())
     finally:
-        utils.sudo_check_call("www-data", 'rm "%s"' % new_extension_messages)
+        mwscript.run_shell(app, "rm {}", new_extension_messages)
 
     # Rebuild all the CDB files for each language
     logger.info(
         "Updating LocalisationCache for %s " "using %s thread(s)" % (version, use_cores)
     )
     _call_rebuildLocalisationCache(
+        app,
         version,
         cache_dir,
         use_cores,
@@ -854,19 +860,23 @@ def disable_service(service, logger=None):
 
 
 @utils.log_context("clear_message_blobs")
-def clear_message_blobs(cfg, logger=None):
+def clear_message_blobs(app, logger=None):
     """
     Clear MessageBlobStore cache on all wikis
 
-    :param cfg: Scap configuration dict
+    :param app: Scap cli.Application
     :param logger: logger instance
     """
-    cmd = "/usr/local/bin/mwscript purgeMessageBlobStore.php"
+    logger.info("Running purgeMessageBlobStore.php")
 
-    logger.info("Running {}".format(cmd))
-
-    # The script affects all wikis
-    utils.sudo_check_call("www-data", cmd)
+    # Note this script affects all wikis
+    mwscript.run(
+        app,
+        "purgeMessageBlobStore.php",
+        wiki=None,
+        network=True,
+        stdout=sys.stdout,
+    )
 
 
 @utils.log_context("port_check")
