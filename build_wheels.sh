cc493c4fdbafdd67d9f91d7cce603d74c0de3650
Ahmon Dancy <adancy@wikimedia.org>
2024-10-15 11:01:32 -0700

    build_wheels.sh: Build wheels a more modern way
    Use the `build` package and `python -m build --wheel` to build the
    Scap wheel.  This avoids the following warning:
    
    ```
    SetuptoolsDeprecationWarning: setup.py install is deprecated.
    !!
    
            ********************************************************************************
            Please avoid running ``setup.py`` directly.
            Instead, use pypa/build, pypa/installer or other
            standards-based tools.
    
            See https://blog.ganssle.io/articles/2021/10/setup-py-deprecated.html for details.
            ********************************************************************************
    
    !!
    ```
    
    Also, announce the major stages of the build process in `build_wheels.sh`.
    
    Change-Id: Icb464953c38c663efa6ce4e5b0699621362ad9b2

--- build_wheels.sh
+++ build_wheels.sh
@@ -4,14 +4,28 @@ set -eu -o pipefail
 
 WHEELS_DIR=wheels
 
+function announce {
+    echo
+    echo "** $* **"
+    echo
+}
+    
+
 # Make sure we're using up-to-date pip, whose wheel will be included
 # for use on installation targets.
+announce Upgrading pip
 pip3 install --upgrade pip
 
+announce Installing build package
+pip3 install build
+
+announce Creating wheels from requirements.txt
 # Create wheels for dependencies
 pip3 wheel -w $WHEELS_DIR \
      pip \
      -r requirements.txt
+
+announce Building the Scap wheel
 # Then create wheel for scap and put everything together
-python3 setup.py bdist_wheel
+python3 -m build --wheel
 mv ./dist/* $WHEELS_DIR
