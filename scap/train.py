wikimedia/mediawiki-tools-scap

commit 9f056763e195741b662fa9474c1ae0dade4ab286

Change the interactions interface
The `Application` class now creates a single interaction object when
needed and uses it for all interactions.  Added new `get_io()`,
`input_line()`, `prompt_choices()`, and
`prompt_user_for_confirmation()` convenience methods.

Change-Id: I6d21195dafdfc987045c9ff64ca6788f1487ecf6

scap/train.py
@@ -2,14 +2,15 @@
 import prettytable
 import re
 
-from scap import cli, utils, tasks, interaction
+from scap import cli, utils, tasks
 
 GROUPS = ["testwikis", "group0", "group1", "group2"]
 
 
 class TrainInfo:
-    def __init__(self, config):
+    def __init__(self, config, io):
         self.config = config
+        self.io = io
         self.train_version = self.get_train_version()
         self.groups = dict()
         self.train_is_at = None
@@ -83,7 +84,7 @@ ____
         version = train_info["version"]
 
         if status not in ["open", "progress"]:
-            check_status = interaction.prompt_user_for_confirmation(
+            check_status = self.io.prompt_user_for_confirmation(
                 f"Train task {task} has status '{status}'. Continue anyway?"
             )
             if not check_status:
@@ -132,7 +133,7 @@ class Train(cli.Application):
         if self.arguments.forward and self.arguments.backward:
             raise SystemExit("Please choose one of --forward or --backward")
 
-        info = TrainInfo(self.config)
+        info = TrainInfo(self.config, self.get_io())
         train_version = info.train_version
         self.old_version = info.get_prior_version()
 
@@ -166,7 +167,7 @@ class Train(cli.Application):
             choices = {stop: str(index) for index, stop in enumerate(stops)}
             choices["Cancel"] = "c"
             default = str(next_pos) if next_pos < len(stops) else "c"
-            target_pos = interaction.prompt_choices(
+            target_pos = self.prompt_choices(
                 f"What station do you want the {train_version} train to be at?",
                 choices,
                 default,

scap/cli.py
@@ -56,6 +56,7 @@ class Application(object):
     program_name = None
     _logger = None
     _announce_logger = None
+    _io = None
     _have_announced = False
     _stats = None
     arguments = None
@@ -233,12 +234,29 @@ class Application(object):
             return self.arguments.message
         return "(no justification provided)"
 
+    # Interaction stuff
+    def get_io(self):
+        if self._io is None:
+            self._io = interaction.GetIO()
+        return self._io
+
+    def input_line(self, prompt: str) -> str:
+        return self.get_io().input_line(prompt)
+
     def prompt_for_approval_or_exit(self, prompt_message, exit_message):
         """Exits successfully with a message if the user does not approve."""
-        approval = interaction.prompt_user_for_confirmation(prompt_message)
+        approval = self.get_io().prompt_user_for_confirmation(prompt_message)
         if not approval:
             self.announce_final(exit_message)
             sys.exit(os.EX_OK)
+
+    def prompt_choices(self, question: str, choices, default=None) -> str:
+        return self.get_io().prompt_choices(question, choices, default)
+
+    def prompt_user_for_confirmation(self, prompt_message, default="n") -> bool:
+        return self.get_io().prompt_user_for_confirmation(prompt_message, default)
+
+    # End interaction stuff
 
     def _process_arguments(self, args, extra_args):
         """

scap/deploy.py
@@ -43,7 +43,6 @@ import scap.nrpe as nrpe
 import scap.script as script
 import scap.template as template
 import scap.cli as cli
-import scap.interaction as interaction
 import scap.log as log
 import scap.ssh as ssh
 import scap.targets as targets
@@ -754,9 +753,7 @@ For `scap deploy` to work, the current directory must be the top level of a git 
                 # T376996
                 self.arguments.message = "scap deploy --init mode"
             else:
-                new_message = interaction.input_line(
-                    "Log message (press enter for none): "
-                )
+                new_message = self.input_line("Log message (press enter for none): ")
                 if new_message:
                     self.arguments.message = new_message
 
@@ -923,7 +920,7 @@ For `scap deploy` to work, the current directory must be the top level of a git 
         except DeployGroupFailure as failure:
             logger.error(str(failure))
 
-            if interaction.prompt_user_for_confirmation(
+            if self.prompt_user_for_confirmation(
                 "Rollback all deployed groups?", default="y"
             ):
                 # Rollback groups in reverse order
@@ -1015,7 +1012,7 @@ For `scap deploy` to work, the current directory must be the top level of a git 
             ):
                 prompt = "Continue?"
                 choices = {"Yes": "y", "No": "n", "Continue all groups": "c"}
-                answer = interaction.prompt_choices(prompt, choices, "y")
+                answer = self.prompt_choices(prompt, choices, "y")
 
                 if answer == "c":
                     self.continue_all = True

scap/deploy_promote.py
@@ -38,7 +38,7 @@ from functools import partial
 import requests
 from requests import RequestException, HTTPError
 
-from scap import cli, utils, config, git, train, interaction
+from scap import cli, utils, config, git, train
 from scap.runcmd import gitcmd
 from scap.utils import BRANCH_RE_UNANCHORED
 
@@ -132,7 +132,7 @@ class DeployPromote(cli.Application):
                 prev_version,
                 self.promote_version,
             )
-        return interaction.prompt_user_for_confirmation(prompt_message)
+        return self.prompt_user_for_confirmation(prompt_message)
 
     def _update_versions(self):
         self._set_messages()

scap/install_world.py
@@ -31,7 +31,7 @@ from os.path import expanduser
 
 import packaging.version
 
-from scap import cli, targets, utils, ssh, log, interaction
+from scap import cli, targets, utils, ssh, log
 from scap.lock import Lock
 
 
@@ -119,7 +119,7 @@ class InstallWorld(cli.Application):
             if total_install_hosts == 0:
                 utils.abort("No hosts to install. Nothing to do")
 
-            if not self.arguments.yes and not interaction.prompt_user_for_confirmation(
+            if not self.arguments.yes and not self.prompt_user_for_confirmation(
                 f"""Scap version "{self.version}" will be installed on {total_install_hosts} host(s). Proceed?"""
             ):
                 utils.abort("Canceled by user")

scap/interaction.py
@@ -25,72 +25,74 @@ def spiderpig_mode() -> bool:
     return bool(os.environ.get("SPIDERPIG_IO_KEY"))
 
 
-def _get_io():
-    return SpiderpigInteraction if spiderpig_mode() else TerminalInteraction
+def GetIO():
+    cls = SpiderpigIO if spiderpig_mode() else TerminalIO
+    return cls()
 
 
-def input_line(prompt: str) -> str:
-    if not interactive():
-        return ""
+class UserIOBase:
+    def output_line(self, line: str):
+        # This base implementation is suitable for TerminalIO and SpiderpigIO
+        print(line)
 
-    return _get_io().input_line(prompt)
+    def input_line(self, prompt: str) -> str:
+        if not interactive():
+            return ""
 
+        return self._input_line(prompt)
 
-def prompt_choices(question: str, choices, default=None) -> str:
-    """
-    Use the 'choices' dict to present a list of choices to the user.
-    If 'choices' is the class 'bool', then choices is defined to be
-    {"Yes": "y", "No": "n"}.
+    def prompt_choices(self, question: str, choices, default=None) -> str:
+        """
+        Use the 'choices' dict to present a list of choices to the user.
+        If 'choices' is the class 'bool', then choices is defined to be
+        {"Yes": "y", "No": "n"}.
 
-    'choices' maps choice descriptions to the key the user should enter to
-    select that choice, for example: {"Continue": "c", "Exit": "e"}.
+        'choices' maps choice descriptions to the key the user should enter to
+        select that choice, for example: {"Continue": "c", "Exit": "e"}.
 
-    Returns the valid choice that the user made.  Re-prompts if an
-    invalid choice is made.  If `default` is supplied, it will be returned
-    if the terminal is not interactive, or if the user just hits enter.
-    """
+        Returns the valid choice that the user made.  Re-prompts if an
+        invalid choice is made.  If `default` is supplied, it will be returned
+        if the terminal is not interactive, or if the user just hits enter.
+        """
 
-    if not interactive() and default:
-        # NOTE: No prompt is seen at all under these circumstances.
-        return default
-
-    if choices == bool:
-        choices = YES_NO_CHOICES
-
-    while True:
-        resp = _get_io().prompt_choices(question, choices, default).strip()
-        if not resp and default:
+        if not interactive() and default:
+            # NOTE: No prompt is seen at all under these circumstances.
             return default
 
-        if resp in choices.values():
-            return resp
-        if resp:
-            print(f"Invalid choice: {repr(resp)}")
+        if choices == bool:
+            choices = YES_NO_CHOICES
+
+        while True:
+            resp = self._prompt_choices(question, choices, default).strip()
+            if not resp and default:
+                return default
+
+            if resp in choices.values():
+                return resp
+            if resp:
+                self.output_line(f"Invalid choice: {repr(resp)}")
+
+    def prompt_user_for_confirmation(self, prompt_message, default="n") -> bool:
+        """
+        Prompts user with `prompt_message` and expects yes/no answer.
+        """
+        return self.prompt_choices(prompt_message, bool, default) == "y"
 
 
-def prompt_user_for_confirmation(prompt_message, default="n") -> bool:
-    """
-    Prompts user with `prompt_message` and expects yes/no answer.
-    """
-    return prompt_choices(prompt_message, bool, default) == "y"
-
-
-class TerminalInteraction:
-    @classmethod
-    def input_line(cls, prompt) -> str:
+class TerminalIO(UserIOBase):
+    def _input_line(self, prompt) -> str:
         return input(prompt)
 
-    @classmethod
-    def prompt_choices(cls, question, choices, default) -> str:
+    def _prompt_choices(self, question, choices, default) -> str:
         """
         Use the 'choices' dict to present a list of choices to the user.
         Returns the user's response.  Validation of the response is not
         performed.
         """
-        return input(cls.generate_prompt_text(question, choices, default))
+        return input(self.generate_prompt_text(question, choices, default))
 
     @classmethod
-    def generate_prompt_text(cls, question, choices, default=None) -> str:
+    def generate_prompt_text(self, question, choices, default=None) -> str:
         if choices == YES_NO_CHOICES:
             if default == "y":
                 prompt = "[Y/n]"
@@ -114,11 +116,9 @@ class TerminalInteraction:
         return f"{choices_text}{question}{default_text}: "
 
 
-class SpiderpigInteraction:
-    @classmethod
-    def input_line(cls, prompt) -> str:
+class SpiderpigIO(UserIOBase):
+    def _input_line(self, prompt) -> str:
         return spiderpigio.input_line(prompt)
 
-    @classmethod
-    def prompt_choices(cls, question, choices, default):
+    def _prompt_choices(self, question, choices, default):
         return spiderpigio.prompt_choices(question, choices, default)

scap/lock.py
@@ -14,7 +14,6 @@ import signal
 import threading
 import time
 
-import scap.interaction as interaction
 import scap.utils as utils
 
 
@@ -302,7 +301,7 @@ class Lock:
             )
 
     @staticmethod
-    def signal_gl_release(release_reason):
+    def signal_gl_release(release_reason, io):
         if os.path.exists(GLOBAL_LOCK_FILE):
             with open(GLOBAL_LOCK_FILE, encoding="UTF-8") as f:
                 lock_info = json.loads(f.read())
@@ -318,7 +317,7 @@ class Lock:
                     + "  reason: %s\n" % reason
                     + "Clear lock?"
                 )
-                if not interaction.prompt_user_for_confirmation(prompt):
+                if not io.prompt_user_for_confirmation(prompt):
                     utils.abort("Canceled by user")
 
                 with utils.empty_file_mask():

scap/main.py
@@ -38,7 +38,6 @@ import scap.arg as arg
 import scap.checks as checks
 import scap.cli as cli
 import scap.git as git
-import scap.interaction as interaction
 import scap.lint as lint
 import scap.lock as lock
 import scap.log as log
@@ -122,7 +121,7 @@ class AbstractSync(cli.Application):
         self._assert_auth_sock()
 
         if self.arguments.message == "(no justification provided)":
-            new_message = interaction.input_line("Log message (press enter for none): ")
+            new_message = self.input_line("Log message (press enter for none): ")
             if new_message:
                 self.arguments.message = new_message
 
@@ -638,7 +637,7 @@ class AbstractSync(cli.Application):
             if test_func():
                 break
 
-            resp = interaction.prompt_choices(
+            resp = self.prompt_choices(
                 "What do you want to do?",
                 {
                     f"Retry {description}": "r",
@@ -1437,7 +1436,7 @@ class LockManager(cli.Application):
                 return 1
 
             self.announce(f"Forcefully removing global lock: {self.arguments.message}")
-            lock.Lock.signal_gl_release(self.arguments.message)
+            lock.Lock.signal_gl_release(self.arguments.message, self.get_io())
             return
 
         if self.arguments.message == "(no justification provided)":

commit 34fd5e6c0dfb9319714ff6f10cad1b858ff73979

scap train: Fix bug in final visualization
Fix bug introduced in 9f056763

Change-Id: I51a134d18d234194a410440df48471624dcd04d8

scap/train.py
@@ -197,7 +197,7 @@ class Train(cli.Application):
             self._deploy_promote(stops[target_pos], train_version)
 
         # Re-read train info and show final visualization
-        TrainInfo(self.config).visualize()
+        TrainInfo(self.config, self.get_io()).visualize()
 
     def _deploy_promote(self, group, version):
         args = []