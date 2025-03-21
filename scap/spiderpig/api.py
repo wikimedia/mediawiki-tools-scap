645b715732e97c448547cd85ce661927707bb167
Ahmon Dancy <adancy@wikimedia.org>
2025-03-21 09:20:36 -0700

    spiderpig: whoami route debugging
    Change-Id: I235011534c090339aa452ad4ff0a5fc8b3285116

--- scap/spiderpig/api.py
+++ scap/spiderpig/api.py
@@ -948,6 +948,8 @@ async def whoami(
 
     return {
         "user": None,
+        "sessionHasUserKey": bool(request.session.get("user")),
+        "sessionEpochOK": bool(request.session.get("epoch") == get_current_epoch()),
         "loginUrl": cas_client(request).get_login_url(),
     }
 
