ebe62d0804cc95df8f1e5bcee61c9e04d7ed8446
Bryan Davis <bd808@wikimedia.org>
2025-04-04 17:27:01 -0600

    spiderpig: manually split vendor chunks
    * Add rollup-plugin-visualizer to produce a dist/chunk-stats.html report
      on built modules.
    * Manually tune Rollup's chunk creation. New chunks are emitted for core
      Vue dependencies (@vue, pinia, vue-router) and @xterm.
    
    Change-Id: I94492fc12f3a29f4cf1d997e332ef5f26acb389c

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,15 +1,43 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
 export default defineConfig( {
 	plugins: [
+		splitVendorChunkPlugin(),
+		visualizer( {
+			emitTile: true,
+			filename: 'dist/chunk-stats.html'
+		} ),
 		vue(),
 		vuetify()
 	],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks( id ) {
+					// Create separate chunks for some large dependencies
+					if (
+						id.includes( '@vue' ) ||
+						id.includes( 'pinia' ) ||
+						id.includes( 'vue-router' )
+					) {
+						return '@vue';
+					}
+					if ( id.includes( '@wikimedia' ) ) {
+						return '@wikimedia';
+					}
+					if ( id.includes( '@xterm' ) ) {
+						return '@xterm';
+					}
+				}
+			}
+		}
+	},
 	resolve: {
 		alias: {
 			'@': fileURLToPath( new URL( './src', import.meta.url ) )
