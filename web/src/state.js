487017b70938bd54dd2aa564d7dc46606a6d1f99
Ahmon Dancy <adancy@wikimedia.org>
2025-05-13 09:24:09 -0700

    web/state/state.js: Add debugging
    Add several `console.log()` calls to collect information to debug T394033.
    
    Bug: T394033
    Change-Id: Ia369467bf3c05e144c41368e42bf61df33e2099b

--- web/src/state.js
+++ web/src/state.js
@@ -11,6 +11,7 @@ export const notificationsStore = defineStore( 'spiderpig-notifications',
 		},
 		actions: {
 			async setUserNotifications( jobId ) {
+				console.log( `setUserNotifications called with jobId: ${ jobId } of type ${ typeof ( jobId ) }` );
 				this.reset();
 				if ( Notification.permission === 'default' ) {
 					await Notification.requestPermission();
@@ -20,14 +21,17 @@ export const notificationsStore = defineStore( 'spiderpig-notifications',
 				}
 			},
 			userShouldBeNotified( interaction ) {
+				console.log( `userShouldBeNotified: Notification.permission is ${ Notification.permission }, interaction.job_id is ${ interaction.job_id }, of type ${ typeof ( interaction.job_id ) }` );
 				return Notification.permission === 'granted' &&
 				interaction.job_id === this.backportJob &&
 				!this.alreadyNotified( interaction.id );
 			},
 			alreadyNotified( interactionId ) {
+				console.log( ` alreadyNotified: interactionId is ${ interactionId }, of type ${ typeof ( interactionId ) }` );
 				return JSON.parse( this.alreadyNotifiedInters ).includes( interactionId );
 			},
 			rememberNotified( interactionId ) {
+				console.log( ` rememberNotified: interactionId is ${ interactionId }, of type ${ typeof ( interactionId ) }` );
 				const alreadyNotified = JSON.parse( this.alreadyNotifiedInters );
 				alreadyNotified.push( interactionId );
 				this.alreadyNotifiedInters = JSON.stringify( alreadyNotified );
