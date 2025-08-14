b394e0ceb8ba6652be82b1ca132d7857b8e7a39d
Ahmon Dancy <adancy@wikimedia.org>
2025-08-14 09:37:29 -0700

    patches.py: Add "next" handling to update-patch/remove-patch
    Augment `scap update-patch` and `scap remove-patch` so that they have
    special handling for "next" patches while retaining their old behavior
    for weekly train branches.
    
    If the target patch is in `/srv/patches/next`, then
    `update_next_patches()` as the start of a `scap update-patch` or
    `scap remove-patch` operation.
    
    `update-patch`:
    
    Installs the patch as an `.updated` patch if it differs from the
    corresponding patch that would be inherited from this week's train.
    If the installed patch is the same as the would-be-inherited patch,
    then use the regular filename (which means that inheritance is
    retained).
    
    `remove-patch`:
    
    If the target patch is in `/srv/patches/next`, replace the patch with
    a `.dropped` patch.
    
    Other stuff:
    
    * Added `_gitcmd()` to factor out a common pattern.
    
    Bug: T295925
    Change-Id: Idf23ea8243152c1cd73dabed806f989405133514

--- scap/patches.py
+++ scap/patches.py
@@ -344,6 +344,11 @@ class PatchUserManipulation(cli.Application):
                 utils.abort(f"git is not clean: {self.config['patch_path']}")
 
             git.set_env_vars_for_user()
+
+            if self.target_version == "next":
+                update_next_patches(self.config["patch_path"], self.get_logger())
+                self._sanity_check_next_patches()
+
             if not os.path.isdir(self.target_version_dir):
                 self.get_logger().info(
                     f"Directory for version {self.target_version} does not exist yet and will be created"
@@ -362,13 +367,16 @@ class PatchUserManipulation(cli.Application):
             ]
             if self.arguments.message_body:
                 commit_message += ["-m", self.arguments.message_body]
-            gitcmd("commit", *commit_message, cwd=self.config["patch_path"])
+            self._gitcmd("commit", *commit_message)
 
             self.get_logger().info(
                 f"Patch {self.arguments.patch_path} {self._operation_name()}d"
             )
         except Exception as e:
             utils.abort(f"Failed to {self._operation_name()} patch: {e}")
+
+    def _gitcmd(self, *args):
+        gitcmd(*args, cwd=self.config["patch_path"])
 
     def _post_init(self):
         """
@@ -406,15 +414,32 @@ class PatchUserManipulation(cli.Application):
         shutil.copytree(latest_patches, self.target_version_dir)
 
         self._git_add_patch_changes()
-        gitcmd(
+        self._gitcmd(
             "commit",
             "-m",
             f"Scap {self._operation_name()}-patch: initial patches for {self.target_version}",
-            cwd=self.config["patch_path"],
         )
 
     def _git_add_patch_changes(self):
-        gitcmd("add", "--all", cwd=self.config["patch_path"])
+        self._gitcmd("add", "--all")
+
+    def _sanity_check_next_patches(self):
+        dropped = self.arguments.patch_path + ".dropped"
+        updated = self.arguments.patch_path + ".updated"
+
+        dropped_exists = os.path.exists(dropped)
+        updated_exists = os.path.exists(updated)
+        patch_path_exists = os.path.exists(self.arguments.patch_path)
+
+        # Sanity checks
+        if dropped_exists and updated_exists:
+            raise Exception(
+                f"Invalid state: {dropped} and {updated} should not both exist"
+            )
+        if (dropped_exists or updated_exists) and patch_path_exists:
+            raise Exception(
+                f"Invalid state: {dropped} or {updated} should not exist if {self.arguments.patch_path} exists"
+            )
 
     @abstractmethod
     def _operation_on_patch(self):
@@ -433,7 +458,7 @@ class UpdatePatch(PatchUserManipulation):
     The MediaWiki version directory is not required to exist yet, in that case a
     new directory for the version will be created and populated with the patches
     from the most recent available version. The specified patch will then be
-    updated
+    updated.
 
     Example usage:
     scap update-patch --message-body "Rebase to solve merge conflicts with mainline code" \\
@@ -458,10 +483,67 @@ class UpdatePatch(PatchUserManipulation):
         return super().main(*extra_args)
 
     def _operation_on_patch(self):
+        if self.target_version != "next":
+            os.makedirs(os.path.dirname(self.arguments.patch_path), exist_ok=True)
+            shutil.copyfile(
+                self.arguments.revised_patch_path.name, self.arguments.patch_path
+            )
+            return
+
+        # "next" handling here.
+
+        # Scenarios with invalid states removed:
+        # # | patch_patch exists | .dropped exists | .updated exists | Result
+        # 1 | no                 | no              | no              | Copy the input file to the .updated file.  Then compare the updated file
+        #                                                            | with this week's train patch (if it exists).
+        #                                                            | If different, then done.  If the same, then we no longer need the .updated
+        #                                                            | file.  Rename it back to the normal name to reactivate inheritance.
+        # 2 | no                 | no              | yes             | Same as scenario 1
+        # 3 | no                 | yes             | no              | Remove the .dropped file, then proceed to scenario 1
+        # 4 | yes                | no              | no              | Rename patch_file to .updated, then proceed to scenario 1
+
+        dropped = self.arguments.patch_path + ".dropped"
+        updated = self.arguments.patch_path + ".updated"
+
+        dropped_exists = os.path.exists(dropped)
+        patch_path_exists = os.path.exists(self.arguments.patch_path)
+
+        logger = self.get_logger()
+
+        if dropped_exists:
+            # Scenario 3
+            logger.info(f"Removing {dropped}")
+            self._gitcmd("rm", dropped)
+
+        if patch_path_exists:
+            # Scenario 4
+            logger.info(f"Renaming inherited {self.arguments.patch_path} to {updated}")
+            self._gitcmd("mv", self.arguments.patch_path, updated)
+
+        # Scenario 1
+        logger.info(f"Copying {self.arguments.revised_patch_path.name} to {updated}")
         os.makedirs(os.path.dirname(self.arguments.patch_path), exist_ok=True)
-        shutil.copyfile(
-            self.arguments.revised_patch_path.name, self.arguments.patch_path
+        shutil.copyfile(self.arguments.revised_patch_path.name, updated)
+        # Preemptively git add the .update file in case it needs to be git mv'd below.
+        self._gitcmd("add", updated)
+
+        current_patches_dir = utils.select_latest_patches(
+            self.config["patch_path"], before_next=True
         )
+        if current_patches_dir:
+            corresponding_current_patch = os.path.join(
+                current_patches_dir, self.module, self.patch_name
+            )
+
+            if os.path.exists(corresponding_current_patch) and filecmp.cmp(
+                updated, corresponding_current_patch
+            ):
+                # The updated file is the same the corresponding patch
+                # for this week's train, so reestablish inheritance.
+                logger.info(
+                    f"{updated} is the same as {corresponding_current_patch}. Renaming to {self.arguments.patch_path} to reestablish inheritance."
+                )
+                self._gitcmd("mv", updated, self.arguments.patch_path)
 
     def _operation_name(self):
         return "update"
@@ -475,7 +557,7 @@ class RemovePatch(PatchUserManipulation):
     The MediaWiki version directory is not required to exist yet, in that case a
     new directory for the version will be created and populated with the patches
     from the most recent available version. The specified patch will then be
-    removed
+    removed.
 
     Example usage:
     scap remove-patch --message-body "Remove patch already made public" \\
@@ -495,14 +577,47 @@ class RemovePatch(PatchUserManipulation):
         return super().main(*extra_args)
 
     def _operation_on_patch(self):
-        os.unlink(self.arguments.patch_path)
-        # Remove any empty dirs that may have been left behind
-        for d, _, _ in os.walk(self.target_version_dir, topdown=False):
-            try:
-                os.rmdir(d)
-            except OSError:
-                # Dir wasn't empty, ignore
-                pass
+        if self.target_version != "next":
+            self._gitcmd("rm", self.arguments.patch_path)
+            return
+
+        # "next" handling
+
+        # Scenarios with invalid states removed:
+        # patch_patch exists | .dropped exists | .updated exists | Result
+        # no                 | no              | no              | Error: f"{self.arguments.patch_path} does not exist"
+        # no                 | no              | yes             | The user wants to block a previously updated patch. Rename .updated to .dropped.
+        # no                 | yes             | no              | Error: f"{self.arguments.patch_path} has already been dropped"
+        # yes                | no              | no              | The user wants to block an inherited patch.  Rename patch_file to .dropped.
+
+        dropped = self.arguments.patch_path + ".dropped"
+        updated = self.arguments.patch_path + ".updated"
+
+        dropped_exists = os.path.exists(dropped)
+        updated_exists = os.path.exists(updated)
+        patch_path_exists = os.path.exists(self.arguments.patch_path)
+
+        logger = self.get_logger()
+
+        if patch_path_exists:
+            # The user wants to drop an inherited patch. (4th scenario)
+            logger.info(f"Renaming {self.arguments.patch_path} to {dropped}")
+            self._gitcmd("mv", self.arguments.patch_path, dropped)
+            return
+
+        if not dropped_exists and not updated_exists:
+            # (1st scenario)
+            raise Exception(f"{self.arguments.patch_path} does not exist")
+
+        if dropped_exists:
+            # (3rd scenario)
+            raise Exception(f"{self.arguments.patch_path} has already been dropped")
+
+        assert updated_exists
+        # (2nd scenario)
+        # The user wants to block a previously updated patch. Rename .updated to .dropped.
+        logger.info(f"Renaming {updated} to {dropped}")
+        self._gitcmd("mv", updated, dropped)
 
     def _operation_name(self):
         return "remove"
