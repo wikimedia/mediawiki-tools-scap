d0efe4a7582ee03430d7477451dcc6b987d41f2d
Ahmon Dancy <adancy@wikimedia.org>
2025-12-03 08:50:49 -0800

    web: rename NewJobPage -> JobPage
    The novelty has worn off and NewJobPage is no longer new, so use a
    better name.
    
    Change-Id: I9798e8900734b2c98a3f3ce4d3738c5297cb05e6

--- web/src/components/TrainPage.vue
+++ web/src/components/TrainPage.vue
@@ -1,20 +1,20 @@
 <template>
-	<sp-new-job-page>
+	<sp-job-page>
 		<template #new-job>
 			<sp-train />
 		</template>
-	</sp-new-job-page>
+	</sp-job-page>
 </template>
 
 <script type="ts">
 import { defineComponent } from 'vue';
-import SpNewJobPage from './NewJobPage.vue';
+import SpJobPage from './JobPage.vue';
 import SpTrain from './Train.vue';
 
 export default defineComponent( {
 	name: 'TrainPage',
 	components: {
-		SpNewJobPage,
+		SpJobPage,
 		SpTrain
 	}
 } );
