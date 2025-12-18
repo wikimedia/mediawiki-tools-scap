64f6c523103ece3ae0f48209b4cdb43042d71e95
Ahmon Dancy <adancy@wikimedia.org>
2025-12-18 10:14:38 -0800

    Deprecation fixes for trixie
    Bug: T413102
    Change-Id: I57d5eb912ce3a35b8c4ce16f42c0642be3314dea

--- tests/scap/test_tasks.py
+++ tests/scap/test_tasks.py
@@ -24,11 +24,11 @@ def test_handle_services(service_disable_call, on_secondary_host, action):
 
     # Default is restarting the service
     if action == "" or action == "=restart":
-        service_disable_call.called_with(
+        service_disable_call.assert_called_with(
             "sudo -n /usr/sbin/service at_your_service restart".split()
         )
     if action == "=reload":
-        service_disable_call.called_with(
+        service_disable_call.assert_called_with(
             "sudo -n /usr/sbin/service at_your_service reload".split()
         )
     if action == "=reload:disable-secondary":
@@ -40,7 +40,7 @@ def test_handle_services(service_disable_call, on_secondary_host, action):
                 ]
             )
         else:
-            service_disable_call.called_with(
+            service_disable_call.assert_called_with(
                 "sudo -n /usr/sbin/service at_your_service reload".split()
             )
     if action == "=restart:disable-secondary":
@@ -52,6 +52,6 @@ def test_handle_services(service_disable_call, on_secondary_host, action):
                 ]
             )
         else:
-            service_disable_call.called_with(
+            service_disable_call.assert_called_with(
                 "sudo -n /usr/sbin/service at_your_service restart".split()
             )
